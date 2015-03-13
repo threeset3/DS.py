d52a5e4528e343fa3e7c5453217f310d8cef08e7
Joon Hyung Ahn <jahn45@gelib-057-31.ews.illinois.edu>
2015-03-12 19:59:14 -0500

    try this shit again

--- server.py
+++ server.py
@@ -8,102 +8,69 @@ import time
 import datetime
 import random
 import ConfigParser
-server_port = None
+import queue
 
-A_socket = None
-B_socket = None
-C_socket = None
-D_socket = None
+server_port = None
+sock = [None] * 4
+
+# return index of socket letter
+def idx(char):
+	return ord(char[0].lower()) - 98
+
 # Parses the configuration file
 def parse_config():
-	global server_port
 	configParser = ConfigParser.RawConfigParser()
-	ConfigFilePath = r'configurationfile.txt'
+	ConfigFilePath = r'config.txt'
 	configParser.read(ConfigFilePath)
 
 	#get the server port to use later
+	global server_port
 	server_port = configParser.get('server','port')
-	print "server_port: %s" % server_port
-	print "server_port: %d" % int(server_port)
-def clientThread(conn):
-	global done_reg, A_socket, B_socket, C_socket, D_socket
+	print "Server_port: %s" % server_port
+
+# Client receiving thread
+def clientThread(conn, unique):
+	global sock
 	while 1:
+		# continuously receive data from a client
 		data = conn.recv(1024)
-		if(data == 'A'):
-			#store A's socket descriptor
-			A_socket = conn
-			print 'received A\'s regisration'
-		elif(data == 'B'):
-			#store B's socket descriptor
-			B_socket = conn
-			print 'received B\'s regisration'
-		elif(data == 'C'):
-			#store C's socket descriptor
-			C_socket = conn
-			print 'received C\'s regisration'
-		elif(data == 'D'):
-			#store D's socket descriptor
-			D_socket = conn
-			print 'received D\'s regisration'
+
+		# if data is identifying msg
+		if(data == 'A' or data == 'B' or data == 'C' or data == 'D'):
+			sock[idx(data)] = conn
+			print unique + ' identified as ' + data
+
+		# if received actual data
 		elif data != "":
 			print 'Received \"' + data + '\" from server_port ' + str(server_port) + ', Max delay is ? s, ' + ' system time is ' + str(datetime.datetime.now())
-			
+
 			#create a new socket to talk with the final destination client
 			buf = data.split(' ');
-			if buf[1] == 'A':
-				#send to A
-				print 'sending to A'
-				if(A_socket != None):
-					if(A_socket.sendall(buf[0])==None):
+
+			if(buf[1] == 'A' or buf[1] == 'B' or buf[1] == 'C' or buf[1] == 'D'):
+				if(sock[idx(data)] != None):
+					if(sock[idx(data)].sendall(buf[0]) == None):
 						print 'Sent \"' + str(buf[0]) + '\" to A ' + '. The system time is ' + str(datetime.datetime.now())
 					else:
 						print 'message send failure'
 				else:
-					print 'Client doesn\'t exist'
-			elif buf[1] =='B':
-				#send to B
-				print 'sending to B'
-				if(B_socket != None):
-					if(B_socket.sendall(buf[0])==None):
-						print 'Sent \"' + str(buf[0]) + '\" to B ' + '. The system time is ' + str(datetime.datetime.now())
-					else:
-						print 'message send failure'
-				else:
-						print 'Client doesn\'t exist'
-			elif buf[1] =='C':
-				#send to C
-				print 'sending to C'
-				if(C_socket != None):
-					if(C_socket.sendall(buf[0])==None):
-						print 'Sent \"' + str(buf[0]) + '\" to C ' + '. The system time is ' + str(datetime.datetime.now())
-					else:
-						print 'message send failure'
-				else:
-					print 'Client doesn\'t exist'
-			elif buf[1] =='D':
-				#send to D
-				print 'sending to D'
-				if(D_socket != None):
-					if(D_socket.sendall(buf[0])==None):
-						print 'Sent \"' + str(buf[0]) + '\" to D' + '. The system time is ' + str(datetime.datetime.now())
-					else:
-						print 'message send failure'
-				else:
-					print 'Client doesn\'t exist'
+					print 'Client ' + data + 'doesn\'t exist'
 			else:
-				print 'incorrectly formatted message!'
+				print 'Received invalid msg'
+
+
 def server():
-	global s_server, server_port, A_socket, B_socket, C_socket, D_socket, done_reg
+	global s_server, server_port, sock
 	s_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	print 'Socket created'
 		 
+	## setup server socket
 	try:
-		#print("server_port: %d", 8000)
 		if(server_port):
 			s_server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 			s_server.bind(('', int(server_port)))
 		else:
-			print 'server port not given'
+			print 'Server port not given'
 			sys.exit()
 	except socket.error , msg:
 		print 'Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
@@ -116,12 +83,10 @@ def server():
 	while 1:
 		conn, addr = s_server.accept()
 		print 'Connected With '  + addr[0] + ':' + str(addr[1])
-		thread.start_new_thread(clientThread, (conn,))
+		thread.start_new_thread(clientThread, (conn, str(addr[1])))
 
 	conn.close()
 	s_server.close()
 	
-global done_reg
-done_reg = 0;
 parse_config()
 server()
